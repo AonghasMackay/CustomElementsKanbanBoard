ec8da5b6c67a246dc655cae590c8047eda1ae792
AonghasMackay <aonghasmackay@yahoo.co.uk>
2022-12-15 20:09:29 +0000

    add delete column functionality and style kanban-column

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
  * Kanban Board built with Web Components Custom Elements API
  * @version 0.0.1
  * @author Aonghas MacKay
+ * @license GPL-3.0
  * 
- * @todo break custom elements up into seperate module files. See: https://stackoverflow.com/questions/55080103/how-to-separate-web-components-to-individual-files-and-load-them
  * @todo style columns and cards - must be done from within shadow DOM
  * @todo define a custom element for kanban card
  * @todo allow new columns and cards to be added to the board
@@ -17,6 +17,9 @@
 //when dom has loaded...
 document.addEventListener("DOMContentLoaded", function(event) {
     customElements.define('kanban-column', KanbanColumn);
+    customElements.define('kanban-card', KanbanCard, { extends: 'div' });
+
+    new KanbanBoard();
 });
 
 /**
@@ -29,18 +32,63 @@ document.addEventListener("DOMContentLoaded", function(event) {
  * @todo add a new card button functionality
  */
 class KanbanColumn extends HTMLElement {
-    constructor() {
+    constructor(columnName = 'To Do') {
         //Calls the parent class's constructor and binds the parent class's public fields, 
         //after which the derived class's constructor can further access and modify 'this'.
         //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
         super();
 
         //get the template from the dom and clone it
-        const KanbanColumnTemplate = document.getElementById('kanban-column-template');
-        const KanbanColumnClone = KanbanColumnTemplate.content.cloneNode(true);
+        const kanbanColumnTemplate = document.getElementById('kanban-column-template');
+        const kanbanColumnClone = kanbanColumnTemplate.content.cloneNode(true);
 
         //create a shadow DOM root and append the cloned template to it
         const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.appendChild(KanbanColumnClone);
+        shadowRoot.appendChild(kanbanColumnClone);
+
+        this.addColumnEventListeners(shadowRoot);
+        this.setColumnName(shadowRoot, columnName);
+    }
+
+    setColumnName(shadowRoot, columnName) {
+        shadowRoot.querySelector('[name="kanban-column-title"]').textContent = columnName;
+    }
+
+    addColumnEventListeners(shadowRoot) {
+        const kanbanColumn = this;
+        const deleteKanbanColumnButton = shadowRoot.querySelector('.kanban-column-delete-button');
+        deleteKanbanColumnButton.addEventListener('click', function(event) {
+            kanbanColumn.deleteKanbanColumn();
+        });
+    }
+
+    deleteKanbanColumn() {
+        this.remove();
+        return true;
+    }
+}
+
+
+class KanbanCard extends HTMLDivElement {
+    constructor() {
+        //Calls the parent class's constructor and binds the parent class's public fields, 
+        //after which the derived class's constructor can further access and modify 'this'.
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+        super();
+    }
+}
+
+class KanbanBoard {
+    constructor(kanbanColumns = ['To Do', 'In Progress', 'Testing', 'Done']) {
+        const self = this;
+        this.main = document.querySelector('main');
+
+        kanbanColumns.forEach(function(columnName) {
+            self.addKanbanColumn(columnName);
+        });
+    }
+
+    addKanbanColumn(columnName) {
+        this.main.appendChild(new KanbanColumn(columnName));
     }
 }
